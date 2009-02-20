db8283a86773c93b48efef4b01794b40a00c20d2
Jeremy Ashkenas <jashkenas@gmail.com>
2009-02-20 00:04:17 -0500

    with real state names based on postal codes

--- public/javascripts/know-thy-congressman.js
+++ public/javascripts/know-thy-congressman.js
@@ -138,6 +138,28 @@ KTC = {
       '07' : 'Jul', '08' : 'Aug', '09' : 'Sep', '10' : 'Oct', '11' : 'Nov', '12' : 'Dec'
     },
     
+    STATE_MAP : {
+      'AL' : 'ALABAMA', 'AK' : 'ALASKA', 'AS' : 'AMERICAN SAMOA',
+      'AZ' : 'ARIZONA', 'AR' : 'ARKANSAS', 'CA' : 'CALIFORNIA',
+      'CO' : 'COLORADO', 'CT' : 'CONNECTICUT', 'DE' : 'DELAWARE',
+      'DC' : 'DISTRICT OF COLUMBIA', 'FM' : 'FEDERATED STATES OF MICRONESIA',
+      'FL' : 'FLORIDA', 'GA' : 'GEORGIA', 'GU' : 'GUAM', 'HI' : 'HAWAII',
+      'ID' : 'IDAHO', 'IL' : 'ILLINOIS', 'IN' : 'INDIANA', 'IA' : 'IOWA',
+      'KS' : 'KANSAS', 'KY' : 'KENTUCKY', 'LA' : 'LOUISIANA', 'ME' : 'MAINE',
+      'MH' : 'MARSHALL ISLANDS', 'MD' : 'MARYLAND', 'MA' : 'MASSACHUSETTS',
+      'MI' : 'MICHIGAN', 'MN' : 'MINNESOTA', 'MS' : 'MISSISSIPPI',
+      'MO' : 'MISSOURI', 'MT' : 'MONTANA', 'NE' : 'NEBRASKA', 'NV' : 'NEVADA',
+      'NH' : 'NEW HAMPSHIRE', 'NJ' : 'NEW JERSEY', 'NM' : 'NEW MEXICO',
+      'NY' : 'NEW YORK', 'NC' : 'NORTH CAROLINA', 'ND' : 'NORTH DAKOTA',
+      'MP' : 'NORTHERN MARIANA ISLANDS', 'OH' : 'OHIO', 'OK' : 'OKLAHOMA',
+      'OR' : 'OREGON', 'PW' : 'PALAU', 'PA' : 'PENNSYLVANIA',
+      'PR' : 'PUERTO RICO', 'RI' : 'RHODE ISLAND', 'SC' : 'SOUTH CAROLINA',
+      'SD' : 'SOUTH DAKOTA', 'TN' : 'TENNESSEE', 'TX' : 'TEXAS', 'UT' : 'UTAH',
+      'VT' : 'VERMONT', 'VI' : 'VIRGIN ISLANDS', 'VA' : 'VIRGINIA',
+      'WA' : 'WASHINGTON', 'WV' : 'WEST VIRGINIA', 'WI' : 'WISCONSIN', 
+      'WY' : 'WYOMING'
+    },
+    
     KIND_OF_CONGRESSMAN_MAP : {
       'Sen' : 'Senator', 
       'Rep' : 'Representative', 
@@ -185,6 +207,7 @@ KTC = {
       data.kind_of_congressman = this.KIND_OF_CONGRESSMAN_MAP[data.title];
       data.name = data.firstname + " " + data.lastname;
       data.titled_name = data.kind_of_congressman + " " + data.lastname;
+      data.state_name = this.STATE_MAP[data.state];
       data.requested_earmarks = KTC.Util.friendlyMoney(data.amt_earmark_requested);
       data.received_earmarks = this.mungeReceivedEarmarks(data);
       data.born = (KTC.Util.truncate(data.birthplace, 20) || this.UNKNOWN) + ' <small>(' + this.mungeDate(data.birthday) + ")</span>";
@@ -238,7 +261,7 @@ KTC = {
       $.each(edu.replace(/\.$/, '').split(/\W*\n\W*/), function(i, deg) {
         var date = deg.match(/\d+$/);
         deg = deg.replace(/,\s*\d+$/, '');
-        deg += " <small>(" + date.toString() + ")</small>";
+        if (date) deg += " <small>(" + date.toString() + ")</small>";
         if (!deg.match(/honorary/i)) degrees.push(deg);
       });
       return degrees.join("<br />");
