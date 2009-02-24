278f58b1c2ba7d14e61b0a9d195ba97ddeee5040
Jeremy Ashkenas <jashkenas@gmail.com>
2009-02-24 00:03:27 -0500

    adding secrets.yml, against my better judgement, to get it up on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,4 +21,4 @@ config.action_controller.perform_caching             = true
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
-SERVER_ROOT = "http://freezing-frost-97.heroku.com"
+SERVER_ROOT = "http://know-thy-congressman.heroku.com"
