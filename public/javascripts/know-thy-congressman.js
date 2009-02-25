d6e94ea435c7862f959ca26cd2bb00d212c1190d
Jeremy Ashkenas <jashkenas@gmail.com>
2009-02-24 23:14:10 -0500

    changing jquery to use noConflict in order to not totally bork prototype-based websites

--- public/javascripts/know-thy-congressman.js
+++ public/javascripts/know-thy-congressman.js
@@ -27,7 +27,7 @@ KTC = {
       var runnerFunction = function() {
         KTC.Loader.attachLiveFunctions();
         KTC.Loader.loadJavascript(KTC.Loader.urls.jqueryDrag, function() {
-          $('#ktc').draggable();
+          $J('#ktc').draggable();
         });
         KTC.Politician.run();
       };
@@ -35,7 +35,8 @@ KTC = {
       this.loadJavascript(this.urls.templates, loaderFunction);
       this.loadStylesheet(this.urls.stylesheet);
       this.loadJavascript(this.urls.jquery, function() {
-        if ($.browser.msie) {
+        $J = window.jQuery.noConflict();
+        if ($J.browser.msie) {
           KTC.Loader.loadStylesheet(KTC.Loader.urls.iestyles);
           KTC.Loader.loadJavascript(KTC.Loader.urls.excanvas, runnerFunction);
         } else {
@@ -56,15 +57,16 @@ KTC = {
       var html = KTC.templates.searcher({text : text, className : className});
       document.body.innerHTML += html;
       ktc = document.getElementById('ktc');
+      ktc.style.display = 'block';
       KTC.Util.alignElement(ktc);
-      if (window.jQuery && $(ktc).draggable) $(ktc).draggable();
+      if (window.jQuery && $J(ktc).draggable) $J(ktc).draggable();
       if (!text) document.getElementById('ktc_search_input').focus();
     },
     
     
     // Show an error message (like when we can't find a politician...)
     showError : function(data) {
-      var ktc = $('#ktc');
+      var ktc = $J('#ktc');
       ktc.removeClass('searching');
       ktc.addClass('erroring');
       ktc.find('.search_error').html(data.error);
@@ -73,11 +75,11 @@ KTC = {
     
     // Attach all the "live" functions that we want hooked up to the UI.
     attachLiveFunctions : function() {
-      $('#ktc_search_button').live('click', KTC.Politician.run);
-      $('#ktc .closer').live('click', function() { 
-        $.browser.msie ? $('#ktc').hide() : $('#ktc').fadeOut(); 
+      $J('#ktc_search_button').live('click', KTC.Politician.run);
+      $J('#ktc .closer').live('click', function() { 
+        $J.browser.msie ? $J('#ktc').hide() : $J('#ktc').fadeOut(); 
       });
-      $('#ktc_search_input').live('keypress', function(e) {
+      $J('#ktc_search_input').live('keypress', function(e) {
         if (e.which == KTC.Loader.RETURN_KEY) KTC.Politician.run();
       });
     },
@@ -241,14 +243,14 @@ KTC = {
     searchFor : function(name) {
       var data = {name : this.mungeName(name), callback : 'KTC.Politician.loaded'};
       var url = KTC.Loader.urls.politician(data);
-      $.ajax({url : url, dataType : 'script'});
+      $J.ajax({url : url, dataType : 'script'});
     },
     
     
     // After the politician's data has loaded, we can really get started.
     // Munge the data as needed.
     loaded : function(data) {
-      var ktc = $('#ktc');
+      var ktc = $J('#ktc');
       if (ktc.length == 0) return;        // Bail if they've closed the window.
       data = window.eval("("+data+")");
       if (data.error) return KTC.Loader.showError(data);
@@ -259,7 +261,7 @@ KTC = {
       this.element.draggable();
       KTC.Util.alignElement(this.element[0]);
       this.element.hide();
-      $.browser.msie ? this.element.show() : this.element.fadeIn('slow');
+      $J.browser.msie ? this.element.show() : this.element.fadeIn('slow');
     },
     
     
@@ -320,7 +322,7 @@ KTC = {
     mungeEducation : function(edu) {
       if (!edu) return this.UNKNOWN;
       var degrees = [];
-      $.each(edu.replace(/\.$/, '').split(/\W*\n\W*/), function(i, deg) {
+      $J.each(edu.replace(/\.$/, '').split(/\W*\n\W*/), function(i, deg) {
         var date = deg.match(/\d+$/);
         deg = KTC.Util.truncate(deg.replace(/,\s*\d+$/, ''), 95);
         if (date) deg += " <small>(" + date.toString() + ")</small>";
@@ -334,11 +336,11 @@ KTC = {
     mungeWords : function(data) {
       var html = '';
       if (!data.capitol_words || data.capitol_words.length <= 0) return this.UNKNOWN;
-      var counts = $.map(data.capitol_words, function(w){ return w.word_count; });
+      var counts = $J.map(data.capitol_words, function(w){ return w.word_count; });
       var max = KTC.Util.arrayMax(counts);
       var min = KTC.Util.arrayMin(counts);
       var ratio = KTC.Util.computeScalingFactor(30, 0, max, min);
-      $.each(data.capitol_words, function(i, word) { 
+      $J.each(data.capitol_words, function(i, word) { 
         word.klass = 'word_' + (i + 1);
         data[word.klass] = word.word_count;
         word.name = data.name;
@@ -394,8 +396,8 @@ KTC = {
     render : function(data) {
       if (this.element) this.element.remove();
       var html = KTC.templates.base(data);
-      $('body').append(html);
-      this.element = $('#ktc');
+      $J('body').append(html);
+      this.element = $J('#ktc');
       KTC.Util.alignElement(this.element[0], 'offscreen');
       this.element.css({'background-image' : 'url(' + this.BACKGROUND_URL + ')'});
       this.renderBlocks(data);
@@ -407,7 +409,7 @@ KTC = {
     
     // Render all the predefined blocks of information.
     renderBlocks : function(data) {
-      $.each(this.INFO_TO_DISPLAY, function(){ 
+      $J.each(this.INFO_TO_DISPLAY, function(){ 
         KTC.Politician.renderBlock(this, data); 
       });
     },
@@ -415,7 +417,7 @@ KTC = {
     
     // Render all the predefined graphs to draw in the background.
     renderGraphs : function(data) {
-      $.each(this.GRAPHS_TO_DRAW, function(){ 
+      $J.each(this.GRAPHS_TO_DRAW, function(){ 
         KTC.Grapher.visualize(this, data); 
       });
     },
@@ -481,11 +483,11 @@ KTC = {
     // Visualize the data provided according to the meta.
     visualize : function(meta, data) {
       var width = meta.width; var height = meta.height;
-      var blocks = $('#ktc .blocks');
-      var toPrecede = $('#ktc .block.' + meta.before);
+      var blocks = $J('#ktc .blocks');
+      var toPrecede = $J('#ktc .block.' + meta.before);
       toPrecede.before(KTC.templates.canvas(meta));
       // It's absolutely positioned, so we gotta stick it in the right place.
-      var cont = $('#' + meta.id);
+      var cont = $J('#' + meta.id);
       var top = toPrecede.offset().top - blocks.offset().top + 3;
       var left = toPrecede.offset().left - blocks.offset().left;
       cont.css({'margin-top' : top, 'margin-left' : left});
@@ -500,14 +502,14 @@ KTC = {
       p.fillStyle = colors[data.party] || colors['Other'];
       p.strokeWidth = 0;
             
-      var nums = $.map(meta.data, function(name){ return data[name]; });
+      var nums = $J.map(meta.data, function(name){ return data[name]; });
       var scale = height / KTC.Util.arrayMax(nums);
       var segment = width / (nums.length - 1);
       var div = segment * (1 - this.CURVE_FACTOR);
       var prev_x = 0; var prev_y = 0;
       p.beginPath();
       p.moveTo(0, height);
-      $.each(nums, function(i, num) {
+      $J.each(nums, function(i, num) {
         var x = segment * i;
         var y = height - num * scale;
         if (i == 0) p.lineTo(x, y);
@@ -551,7 +553,7 @@ KTC = {
     
     // Get rid of any Spanish-language unicode accents for ASCII lookups.
     removeAccents : function(string) {
-      $.each(this.UNICODE_TO_REPLACE, function(i, pair) {
+      $J.each(this.UNICODE_TO_REPLACE, function(i, pair) {
         string = string.replace(new RegExp(pair[0], 'gi'), pair[1]);
       });
       return string;
@@ -613,12 +615,12 @@ KTC = {
     reloadCss : function(){
       var process = function(win) {
         var links = win.document.getElementsByTagName('link');
-        $.each(links, function(i, link){
+        $J.each(links, function(i, link){
           if (link.rel.toLowerCase().indexOf('stylesheet') < 0 || !link.href) return;
           var href = link.href.replace(/(&|%5C?)forceReload=\d+/,'');
           link.href = href + (href.indexOf('?') >= 0 ? '&' : '?') + 'forceReload=' + (new Date().valueOf());
         });
-        $.each(win.frames, function(i, frame){ process(frame); });
+        $J.each(win.frames, function(i, frame){ process(frame); });
       };
       process(window);
     }
