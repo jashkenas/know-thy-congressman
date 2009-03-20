e20474ebc0552277dc1e836d94c43cb8b6c66eb4
Jeremy Ashkenas <jashkenas@gmail.com>
2009-03-19 20:22:15 -0400

    making watchdog a little safer

--- lib/services/watchdog.rb
+++ lib/services/watchdog.rb
@@ -9,7 +9,8 @@ module Services
       safe_request('watchdog', :ensure => bioguide_id) do
         dog = Net::HTTP.new(URL, 80)
         redirected = dog.get("/p/search.json?bioguideid=#{bioguide_id}")
-        url = "#{redirected.header['location']}.json"      
+        url = "#{redirected.header['location']}.json"
+        return {} if url.blank? 
         data = get_json(url).first
         data.delete 'bills_sponsored'     # Just a buncha URLS, not too useful.
         data.delete 'earmarks_sponsored'  # Always seems to return a list of nils.
