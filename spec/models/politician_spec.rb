e2b576910665f0ed76fdb4d7334fc9d713955f1c
David James <djames@sunlightfoundation.com>
2009-06-11 14:05:07 -0400

    The politican spec now passes

--- spec/models/politician_spec.rb
+++ spec/models/politician_spec.rb
@@ -3,6 +3,8 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Politician do
   before(:each) do
     @valid_attributes = {
+      :first_name => "Yvette",
+      :last_name  => "Clarke"
     }
   end
 
