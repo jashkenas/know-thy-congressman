607d32e56a70455ab3fa2365b5d9fafa4a524685
Jeremy Ashkenas <jashkenas@gmail.com>
2010-02-01 15:43:46 -0500

    fixing error handling

--- lib/services/base.rb
+++ lib/services/base.rb
@@ -7,27 +7,27 @@ module Services
       begin
         JSON.parse(RestClient.get(URI.encode(url)))
       rescue JSON::ParserError => e # Faux-log any failed requests.
-        @data['failed_requests'] ||= []
-        @data['failed_requests'] <<  url
+        Rails.logger.warn("Failed to parse json for: #{url}")
+        Rails.logger.warn(e.message)
         {}
       end
     end
 
     # Ensure that a failed api call doesn't screw up the remainder of our data.
     def safe_request(title, opts={})
-      RAILS_DEFAULT_LOGGER.info "starting: #{title}"
+      Rails.logger.info "starting: #{title}"
       result = {}
       return result if opts.has_key?(:ensure) && !opts[:ensure]
       begin
         result = yield
-        RAILS_DEFAULT_LOGGER.info "done with: #{title}"
+        Rails.logger.info "done with: #{title}"
       rescue RestClient::Exception => e
         result = {}
-        RAILS_DEFAULT_LOGGER.info "error for: #{title} ... #{e}"
+        Rails.logger.info "error for: #{title} ... #{e}"
       end
       result
     end
 
   end
 
-end 
+end
